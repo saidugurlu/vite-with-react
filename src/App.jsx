2997993ea202049564827d421dd712b8e1f5afca
saidugurlu <ugurlumsaid@gmx.de>
2022-04-19 13:45:36 +0200

    app createt

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.scss';
+import data from './data/data.json';
+
+function App() {
+    return (
+        <div className="App">
+            <div>Demo App Vite</div>
+            <img src="./images/code.png" alt=""/>
+
+            <div>
+                First employee: {data.employees[0].firstName}{' '}
+                {data.employees[0].lastName}
+            </div>
+            <div>
+                Second employee: {data.employees[1].firstName}{' '}
+                {data.employees[1].lastName}
+            </div>
+        </div>
+    );
+}
+
+export default App;
