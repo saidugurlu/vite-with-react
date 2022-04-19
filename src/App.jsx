13641b896b208776a6c1d300b3726315ac031f4d
saidugurlu <ugurlumsaid@gmx.de>
2022-04-19 13:46:18 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import './App.scss';
-import data from './data/data.json';
+import "./App.scss";
+import data from "./data/data.json";
 
 function App() {
-    return (
-        <div className="App">
-            <div>Demo App Vite</div>
-            <img src="./images/code.png" alt=""/>
+  return (
+    <div className="App">
+      <div>Demo App Vite</div>
+      <img src="./images/code.png" alt="images" />
 
-            <div>
-                First employee: {data.employees[0].firstName}{' '}
-                {data.employees[0].lastName}
-            </div>
-            <div>
-                Second employee: {data.employees[1].firstName}{' '}
-                {data.employees[1].lastName}
-            </div>
-        </div>
-    );
+      <div>
+        First employee: {data.employees[0].firstName}{" "}
+        {data.employees[0].lastName}
+      </div>
+      <div>
+        Second employee: {data.employees[1].firstName}{" "}
+        {data.employees[1].lastName}
+      </div>
+    </div>
+  );
 }
 
 export default App;
